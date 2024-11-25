a6dbc0190b2c34732c4f20c9a3cc7cc41c66f0e3
Deston Bui <khangduy158@gmail.com>
2024-11-24 19:52:01 -0500

    Revert "Final: completed project"
    This reverts commit fc4d2aeff3caac56c2a27bd14a55b46dbe49339b.

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,35 +1,19 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 export const CartSlice = createSlice({
-  name: "cart",
+  name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-      const existingItem = state.items.find(
-        (item) => item.name === action.payload.name
-      );
-
-      if (existingItem) {
-        existingItem.quantity += 1;
-      } else {
-        state.items.push({ ...action.payload, quantity: 1 });
-      }
+    
     },
     removeItem: (state, action) => {
-      state.items = state.items.filter(
-        (item) => item.name !== action.payload.name
-      );
     },
     updateQuantity: (state, action) => {
-      const existingItem = state.items.find(
-        (item) => item.name === action.payload.name
-      );
 
-      if (existingItem) {
-        existingItem.quantity = action.payload.newQuantity;
-      }
+    
     },
   },
 });
